0035e110cfde7d849caba0eb624da1dbef2ff22e
gjsdevs <gjsdevs@gmail.com>
2019-11-18 15:50:24 +0000

    update to code 1

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -4,32 +4,89 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FizzBuzz
+namespace FizzBuzzExcercies
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
+            for (int i = 1; i <= 300; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
+                if (i % 11 == 0)
                 {
-                    Console.WriteLine("FizzBuzz");
+                    if (i % 13 == 0)
+                    {
+                        Console.WriteLine("FezzBong");
+                    }
+                    else
+                        Console.WriteLine("Bong");
+                }
+
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    if (i % 13 == 0)
+                    {
+                        Console.WriteLine("FizzFezzBuzz");
+                    }
+                    else
+                        Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 7 == 0 && i % 3 == 0)
+                {
+                    if (i % 13 == 0)
+                    {
+                        Console.WriteLine("FizzFezzBang");
+                    }
+                    else
+                        Console.WriteLine("FizzBang");
+                }
+                else if (i % 7 == 0 && i % 5 == 0)
+                {
+                    if (i % 13 == 0)
+                    {
+                        Console.WriteLine("FezzBuzzBang");
+                    }
+                    else
+                        Console.WriteLine("BuzzBang");
                 }
                 else if (i % 3 == 0)
                 {
-                    Console.WriteLine("Fizz");
+                    if (i % 13 == 0)
+                    {
+                        Console.WriteLine("FizzFezz");
+                    }
+                    else
+                        Console.WriteLine("Fizz");
                 }
                 else if (i % 5 == 0)
                 {
-                    Console.WriteLine("Buzz");
+                    if (i % 13 == 0)
+                    {
+                        Console.WriteLine("FezzBuzz");
+                    }
+                    else
+                        Console.WriteLine("Buzz");
                 }
-
+                else if (i % 7 == 0)
+                {
+                    if (i % 13 == 0)
+                    {
+                        Console.WriteLine("FezzBang");
+                    }
+                    else
+                        Console.WriteLine("Bang");
+                }
+                else if (i % 13 == 0)
+                {
+                    Console.WriteLine("Fezz");
+                }
                 else
                 {
                     Console.WriteLine(i);
 
                 }
+
+
 
 
             }
